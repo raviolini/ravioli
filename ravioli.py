b213437b7d3c869fa77fe0f426ae596b595b055e
ZakiNadhif <zaki.nadhif45@gmail.com>
2021-09-23 19:06:51 +0700

    Remove jokes and dead codes in ravioli.py

--- ravioli.py
+++ ravioli.py
@@ -1,11 +1,9 @@
 #! /usr/bin/env python
-import autoabsen
 
-from halo import Halo
+from modules import siakad_auto_attendance
+from siakad_auto_attendance import main
 from art import tprint
-
 from os import system
-import time
 
 def yn_choice(message, default='y'):
     choices = 'Y/n' if default.lower() in ('y', 'yes') else 'y/N'
@@ -16,34 +14,10 @@ def yn_choice(message, default='y'):
 def say_welcome():
     tprint("{*} RAVIOLI")
 
-def download_ram():
-    spinner = Halo(text='Downloading RAM', spinner='dots')
-    spinner.start()
-    time.sleep(2.4)
-    spinner.succeed()
-
-def stop_alien_invasion():
-    spinner = Halo(text = "Stopping imminent alien invasion", spinner='dots')
-    spinner.start()
-    time.sleep(2.4)
-    spinner.succeed()
-
-def hide_homework_folder():
-    spinner = Halo(text = "Hiding homework folder", spinner='dots')
-    spinner.start()
-    time.sleep(2.4)
-    spinner.succeed()
-
 def ask_auto_absen():
     return yn_choice("Start 'auto-absen' sequence")
 
-def start_neofetch():
-    system('neofetch')
-
 if __name__ == '__main__':
     say_welcome()
-    #download_ram()
-    #stop_alien_invasion()
-    #hide_homework_folder()
     if ask_auto_absen():
-        autoabsen.start()
+        siakad_auto_attendance.main.start()
