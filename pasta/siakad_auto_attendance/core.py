54aad25797603fba5f0a28e673a21aa4ccf4fb77
cowdingus <zaki.nadhif45@gmail.com>
2021-10-20 13:42:18 +0700

    Break core.py into core.py, core_browserless.py

--- pasta/siakad_auto_attendance/core.py
+++ pasta/siakad_auto_attendance/core.py
@@ -4,154 +4,170 @@
     This module contains functions essential for the fillng action
 """
 
-from enum import Enum
 import os
-import re
 
-import pickle
-import requests
+from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+from flour import log_neko
 
 from . import utils
+from . import callbacks
+from .globals import ENDPOINTS
 
-ENDPOINTS = {
-    "welcome_page": "https://siswa.smktelkom-mlg.sch.id/welcome",
-    "presence_status": "https://siswa.smktelkom-mlg.sch.id/welcome/get_status_hadir",
-    "presence_fill": "https://siswa.smktelkom-mlg.sch.id/presnow/chsts",
-    "presence_page": "https://siswa.smktelkom-mlg.sch.id/presnow",
-    "login": "https://siswa.smktelkom-mlg.sch.id/login/act_login"
-}
+def try_sign_in():
+    config = utils.load_config()
+    cookies = utils.load_cookies()
 
-SESSION_FILENAME = "session.pkl"
+    if not config:
+        log_neko.message_warn("It seems like you haven't configured this module. Please configure")
+        return False
 
-class Dalu(Enum):
-    """ Dalu enum for choosing between Daring and Luring """
-    DARING = 0
-    LURING = 1
+    if not cookies:
+        log_neko.message_warn("Cookie is empty")
 
-def dalu_to_str(dalu: Dalu) -> str:
-    """ Converts Dalu enum to valid post-able string argument """
-    if dalu == Dalu.DARING:
-        return "daring"
-    if dalu == Dalu.LURING:
-        return "luring"
-    raise RuntimeError("Invalid Dalu enum value")
+    preferred_browser = config["browser"]
+    email = config["email"]
+    password = config["password"]
 
-def load_session():
-    """ Loads session file """
-    with open(SESSION_FILENAME, "rb") as session_file:
-        return pickle.load(session_file)
+    # Try to instantiate webdriver based on string that was set in the config
+    if preferred_browser.lower() not in AVAILABLE_BROWSER:
+        log_neko.message_warn("Preffered browser isn't supported defaulting to firefox (geckodriver)")
+        preferred_browser = "firefox"
 
-def save_session(session: requests.Session):
-    """ Saves session file """
-    with open(SESSION_FILENAME, "wb") as session_file:
-        pickle.dump(session, session_file)
+    log_neko.message_info(f"Preferred web browser identified as {preferred_browser}")
 
-def session_exists():
-    """ Checks if session file exists """
-    return os.path.exists(SESSION_FILENAME)
+    driver = make_webdriver_from_browser_name(preferred_browser)
 
-def set_cookies_to_session(cookies: dict, session: requests.Session):
-    """
-        Set required cookies to session object in order to fill the attendance
-        form
-    """
-    for cookie in cookies:
-        name = cookie["name"]
-        value = cookie["value"]
+    # Load 404 page to be able to attach cookies into browser without triggering errors
+    driver.get("https://siswa.smktelkom-mlg.sch.id/does_not_exist")
 
-        if name in ("ci_session", "kosogha", "loginsiswa_id_siswa"):
-            session.cookies.set(
-                name = name,
-                value = value,
-                path = cookie.get("path"),
-                domain = cookie.get("domain"),
-                secure = cookie.get("secure"),
-                rest = {"HttpOnly": cookie.get("httpOnly")},
-                expires = cookie.get("expiry")
-            )
+    # Clear `noise` cookies
+    driver.delete_all_cookies()
 
-def get_user_agent_from_config() -> str:
-    return utils.load_config().get("user_agent")
+    # Attach cookies from the stored file (if exists)
+    attach_cookies_into_browser(cookies, driver)
 
-def is_present(session: requests.Session):
-    """ Checks the user's presence status """
-    response = session.post(ENDPOINTS["presence_status"])
-    return "masuk" in response.json()["status_presensi"][0].lower()
+    # Go to welcome page to test if it's logged in or not and act accordingly
+    driver.get("https://siswa.smktelkom-mlg.sch.id/welcome")
 
-def get_page_title(response: requests.Response):
-    """ Gets title of a HTML response """
-    response_text = response.text
-    return re.search('<\W*title\W*(.*)</title', response_text, re.IGNORECASE).group(1)
+    if "Login" in driver.title:
+        log_neko.message_info("Sign in needed. Attempting sign in")
+        log_neko.message_info("Please fill in the captcha box")
 
-def is_signed_in(session: requests.Session):
-    """ Checks if the user is already signed in """
-    response = session.get(ENDPOINTS["welcome_page"])
-    page_title = get_page_title(response)
-    return "Login" not in page_title
+        signed_in = fill_login_form_and_wait_for_recaptcha(driver, email, password)
 
-def post_attendance(session: requests.Session, dalu: Dalu):
-    """ Do a post request to fill the attendance """
-    headers = {
-        "Referer": ENDPOINTS["presence_page"]
-    }
-
-    params = {
-        "ijin": (None, "M", None),
-        "dalu": (None, dalu_to_str(dalu), None),
-    }
-
-    return session.post(
-        url = ENDPOINTS["presence_fill"],
-        files = params,
-        headers = headers,
-    )
-
-def load_presence_page(session: requests.Session):
-    """ Load the presence page in order to get attendance token """
-    headers = {
-        "Referer": ENDPOINTS["welcome_page"]
-    }
-
-    return session.get(
-        url = ENDPOINTS["presence_page"],
-        headers = headers
-    )
-
-def remove_ci_session_cookie(session: requests.Session):
-    """
-        Remove ci_session cookie to prevent it from getting outdated. This way
-        ci_session will be regenerated when accessing ENDPOINTS["presence_page"]
-    """
-    session.cookies.set(name = "ci_session", value = None)
-
-def try_fill_attendance(dalu: Dalu, user_agent: str):
-    """
-        Tries to fill the attendance form for the user
-    """
-    if not session_exists():
-        cookies = utils.load_cookies()
-        session = requests.Session()
-
-        session.headers.update({"User-Agent": user_agent})
-
-        set_cookies_to_session(cookies, session)
+        if not signed_in:
+            log_neko.message_warn("Sign in attempt failed")
+            return False
     else:
-        session = load_session()
+        log_neko.message_warn("The browser's title doesn't contain `Login`. Skipping as signed in")
+        return True
 
-    print("Checking if the login session is valid...")
-    print("Signed In:", is_signed_in(session))
+    # Save user agent string for browserless use
+    user_agent = get_user_agent_from_browser(driver)
+    utils.add_entry_to_config("user_agent", user_agent)
 
-    print("Attempting to load token...")
-    load_presence_page(session)
+    # Save cookies for future (browserless) use
+    utils.save_cookies(driver.get_cookies())
 
-    print("Attempting fill presence...")
-    post_attendance(session, dalu)
+def fill_login_form_and_wait_for_recaptcha(driver: WebDriver, email: str, password: str) -> bool:
+    """
+        Attempts to sign in into siakad
+    """
+    recaptcha_wait_amount = 999
+    cookie_wait_amount = 60
+    submit_wait_amount = 120
 
-    presence_status = is_present(session)
-    print("Presence:", presence_status)
+    recaptcha_xpath = "//iframe[@title='reCAPTCHA']"
+    recaptcha_frame = driver.find_element_by_xpath(recaptcha_xpath)
 
-    remove_ci_session_cookie(session)
+    email_input_element = driver.find_element_by_name("email")
+    password_input_element = driver.find_element_by_name("password")
+    submit_button_element = driver.find_element_by_name("masuk")
 
-    save_session(session)
+    email_input_element.clear()
+    password_input_element.clear()
 
-    return presence_status
+    email_input_element.send_keys(email)
+    password_input_element.send_keys(password)
+
+    driver.switch_to.frame(recaptcha_frame)
+    recaptcha_status = WebDriverWait(driver, recaptcha_wait_amount).until(callbacks.recaptcha_ok)
+
+    if recaptcha_status is True:
+        log_neko.message_info("ReCAPTCHA is identified as checked, signing in...")
+    else:
+        log_neko.message_warn("Failed to login, please check out the presence form yourself")
+        driver.switch_to.parent_frame()
+        return False
+
+    driver.switch_to.parent_frame()
+    submit_button_element.click()
+
+    WebDriverWait(driver, submit_wait_amount).until(callbacks.redirected_into_welcome_page,
+    """
+    Web driver waited too long for it to be redirected into welcome page.
+    This indicate that either your internet is unstable or you have put
+    the wrong user credentials.
+    """)
+
+    # Waits for the page to load session or authentication cookies
+    WebDriverWait(driver, cookie_wait_amount).until(callbacks.has_needed_cookies, "Needed cookies can't be found")
+
+    return True
+
+def is_signed_in_webdriver(driver: WebDriver) -> bool:
+    """
+        Checks if the user is signed in by going to welcome page and
+        checking if the page has redirected the web browser to the
+        login page.
+
+        NOTE: This method doesn't guarantee it will restore the previous
+        loaded page with the same state as it was before.
+    """
+    previous_url = driver.current_url
+
+    driver.get(ENDPOINTS["welcome_page"])
+    signed_in = "Login" not in driver.title
+
+    driver.get(previous_url)
+    return signed_in
+
+AVAILABLE_BROWSER = ["firefox", "chrome", "edge"]
+def make_webdriver_from_browser_name(browser_name: str):
+    """
+        Instantiate respective webdriver specified in browser_name
+    """
+
+    os.environ['WDM_PRINT_FIRST_LINE'] = 'False' #remove the space from log
+    os.environ['WDM_LOG_LEVEL'] = '0' # Silence the webdriver_manager log
+
+    browser_name = browser_name.lower()
+
+    if browser_name == "firefox":
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    elif browser_name == "chrome":
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif browser_name == "edge":
+        driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+    else:
+        raise RuntimeError("Web browser isn't recognized or supported")
+
+    return driver
+
+def attach_cookies_into_browser(cookies: dict, driver: WebDriver):
+    """
+        Attach cookies into web browser
+    """
+
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+def get_user_agent_from_browser(driver: WebDriver):
+    """ Get user agent from selenium webdriver """
+    return driver.execute_script("return navigator.userAgent")
