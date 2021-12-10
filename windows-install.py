dcbd5919c00d1e3765b11232c3842f603f569f7c
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-10 12:50:24 +0700

    cleanup and ready to tetstdrive tomorrow

--- windows-install.py
+++ windows-install.py
@@ -5,40 +5,36 @@ simple script to "install" the ravioli, it just put the absen startup in the sta
 if __name__ == "__main__":
     import os
     import sys
-    import shutil
     import subprocess
-    import platform
-    import time
 
-    # get the path of target script
-    target_path = "./absen-startup.exe"
+    #targeted file
+    target_file = "absen-startup.exe"
 
-    shortcut = "./absen-startup.lnk"
+    #print target_file if exist
+    if os.path.isfile(target_file):
+        print("[+] file found")
 
-    # get the path to startup folder
-    startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
-    # get the path to shortcut
-    shortcut_path = os.path.join(startup_folder, shortcut)
-    # get the path to target file
-    target_path = os.path.join(startup_folder, target_path)
+    #startup folder
+    startup_folder = os.path.join(os.environ["APPDATA"], "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
+    print("your startup folder : ", startup_folder)
 
-    # if file already exist
-    if os.path.exists(shortcut_path):
-        print("file already exist")
+    #get the current path of targeted file
+    target_file = os.path.abspath(target_file)
+
+    #create shortcut from targeted file
+    try:
+        subprocess.call(["cmd", "/c", "mklink", "/J", os.path.join(startup_folder, "absen-startup.lnk"), target_file])
+        print("[+] shortcut created")
+    except Exception as e:
+        print(e)
         sys.exit(1)
-    # if not
-    else:
-        # create folder if not exist
-        if not os.path.isdir(startup_folder):
-            os.makedirs(startup_folder)
-        # create link
-        subprocess.call(['C:\Windows\System32\cmd.exe', '/c', 'mklink', '/D', shortcut_path, target_path])
-        # check if shortcut exist
-        if os.path.exists(shortcut_path):
-            print("shortcut created")
-            sys.exit(0)
-        else:
-            print("failed to create shortcut")
-            sys.exit(1)
+
+    print("[+] done")
+
+    print("ravioli installed")
+    input("press enter to exit")
+
+
+    
 
     
